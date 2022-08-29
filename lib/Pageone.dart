31069b307eaf2dfcb530abfa2afcd5c91b483f09
kasuhikpuka1998 <kaushikghosh199832@gmail.com>
2022-08-30 00:56:53 +0530

    Custom Marker Added and InfoWindow Added

--- lib/Pageone.dart
+++ lib/Pageone.dart
@@ -1,5 +1,9 @@
+import 'dart:typed_data';
+import 'dart:ui' as ui;
+
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 class GoogleMapPage extends StatefulWidget {
@@ -11,11 +15,40 @@ class GoogleMapPage extends StatefulWidget {
 
 class _GoogleMapPageState extends State<GoogleMapPage> {
   Set<Marker> _allMarkers = {};
+  late BitmapDescriptor mapMarker;
+  Uint8List? markerIMage;
+
+  @override
+  void initState() {
+    // TODO: implement initState
+    setCustomMarker();
+    super.initState();
+  }
+
+  Future<Uint8List> getBytesFromAssets(String path, int width) async {
+    ByteData byteData = await rootBundle.load(path);
+    ui.Codec codec = await ui.instantiateImageCodec(
+        byteData.buffer.asUint8List(),
+        targetHeight: width);
+    ui.FrameInfo fi = await codec.getNextFrame();
+
+    return (await fi.image.toByteData(format: ui.ImageByteFormat.png))!
+        .buffer
+        .asUint8List();
+  }
+
+  void setCustomMarker() async {
+    markerIMage = await getBytesFromAssets("images/red_marker.png", 100);
+  }
+
+  void abc() async {}
+
   void _onMarkercreated(GoogleMapController googleMapController) {
     setState(() {
       _allMarkers.add(Marker(
           markerId: MarkerId("id-1"),
           position: LatLng(22.5448131, 88.3403691),
+          icon: BitmapDescriptor.fromBytes(markerIMage!),
           infoWindow: InfoWindow(title: 'Your Location', snippet: 'Position')));
     });
   }
--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
-          primarySwatch: Colors.purple,
+          primarySwatch: Colors.yellow,
           visualDensity: VisualDensity.adaptivePlatformDensity),
       home: HomePage(),
     );
